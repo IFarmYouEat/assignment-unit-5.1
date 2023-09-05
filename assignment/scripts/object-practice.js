7870e1fe7eb7bbd1267ddb864c6c7d4e5d831f38
Brock Nelson <Bmnelson87@gmail.com>
2023-09-05 13:59:28 -0500

    base mode cmoplete and logging object to Dom

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -1,5 +1,7 @@
 console.log('***** Object Practice *****')
 
+let personList = document.querySelector('#person-attributes');
+
 // Add the required code to complete the tasks below
 
 /* 1. Create an object literal to represent you!
@@ -10,6 +12,11 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: "Brock",
+  lastName: "Nelson",
+  hasSiblings: true,
+  shoeCount: 8,
+  favThreeFoods: ["Bagel", "Steak", "Pizza"]
 };
 console.log('A little about me:', me);
 
@@ -20,6 +27,8 @@ console.log('A little about me:', me);
   - Console.log fullName
 */
 
+let fullName = me.firstName + " " + me.lastNAme;
+console.log(fullName);
 
 
 /* 3. Nested arrays
@@ -27,6 +36,8 @@ console.log('A little about me:', me);
   - Console.log your last favorite food 
 */
 
+console.log(me.favThreeFoods[0])
+console.log(me.favThreeFoods[me.favThreeFoods.length -1])
 
 
 /* 4. Change a property of an existing object.
@@ -36,6 +47,14 @@ console.log('A little about me:', me);
   - Console.log your updated shoe count. 
 */
 
+me.favThreeFoods[0] = "Captain Crunch";
+console.log(me.favThreeFoods)
+
+console.log(me.shoeCount);
+me.shoeCount ++;
+console.log(me.shoeCount);
+
+
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +62,18 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = "Ultramarine Blue";
+console.log(me.favoriteColor);
+
+personList.innerHTML += `
+  <h2> My first name is: ${me.firstName} ${me.lastName}.</h2>
+  <p> My favorite color is: ${me.favoriteColor}.</p>
+  <p> I have siblings: ${me.hasSiblings}.</p>
+  <p> I currently have ${me.shoeCount} pairs of shoes.</p>
+`
+for(let i of me.favThreeFoods){
+  personList.innerHTML += `
+  <p> One of my favorite foods is ${i}.</p>
+  `
+}
